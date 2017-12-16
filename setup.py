637be3f274b2cc66df8b6cbc3d18864f66b6c403
Neill Herbst <n.herbst6@gmail.com>
2017-12-16 14:56:09 +0200

    changed install name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bitfinex',
+    name='bitfinex_api',
     version='0.0.1',
     description='package containing the API endpoints of Bitfinex',
     url='https://github.com/NeillHerbst/bitfinex-python.git',
@@ -9,5 +9,8 @@ setup(
     author_email='n.herbst6@gmail.com',
     license='MIT',
     packages=find_packages(),
-    install_requires = []
+    install_requires = [
+        'requests==2.14.2',
+        'websocket-client==0.44.0'
+    ]
 )
